0e40cbd7176a0c7291389f75aa4d97cc0f8c95fb
douniakritet <kritetdounia@gmail.com>
2025-11-11 10:19:58 +0100

    regroupement

--- frontend/src/api/http.ts
+++
@@ -1,6 +0,0 @@
-import axios, { type AxiosInstance } from "axios";
-
-export const http: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8081",
-    headers: { "Content-Type": "application/json" },
-});
--- frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
-    theme: { extend: {} },
-    plugins: [],
-    safelist: [
-        { pattern: /bg-(blue|green|red|purple)-500/ },
-    ],
-};
