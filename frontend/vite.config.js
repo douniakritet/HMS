fa842ca77f7644a19322e1678da5540a57726aaa
nasssima2002 <nassimataarabet2003@gmail.com>
2025-11-14 11:54:44 +0100

    Sauvegarde des changements avant switch vers billing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +10,28 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+   server: {
+    proxy: {
+      '/api/patient': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/patient/, ''),
+      },
+      '/api/doctor': {
+        target: 'http://localhost:8082',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/doctor/, ''),
+      },
+      '/api/appointment': {
+        target: 'http://localhost:8083',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/appointment/, ''),
+      },
+      '/api/billing': {
+        target: 'http://localhost:8084',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/billing/, ''),
+      },
+    }
+  }
 })
